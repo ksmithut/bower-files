3add2c0ab1f841a50217cf991e869939a5d3cab0
Keith Smith <ksmith.ut@gmail.com>
2014-09-24 22:41:48 -0600

    initial rewrite of tests

--- test/bower-files-test.js
+++ test/bower-files-test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var fs     = require('fs');
-var should = require('should');
 var path   = require('path');
+var expect = require('expect.js');
 var cwd    = process.cwd();
 
 // Tests
@@ -12,26 +12,122 @@ describe('bower-files tests', function () {
   // After each, reset the `process.cwd()`
   afterEach(function () {
     cd('../');
-    deleteCache('bower-files.js');
-    deleteCache('defaults.js');
-    deleteCache('exists.js');
-    deleteCache('resolve-path.js');
+    deleteCache('index.js');
   });
 
   describe('Level 1', function () {
-    it('should use the override', override);
-    it('should handle glob files', globs);
-    it('should get list of files without being split by extension', justArray);
-    it('should get only the files with a certain extension', justOneExt);
-    it('should get devDependencies when option is given', devDependencies);
-    it('should not get devDependencies when option is not given', noDevDependencies);
+
+    it('should use the override', function () {
+      cd('override');
+      var files        = getModule();
+      var pathToJquery = './override/bower_components/jquery/dist/jquery.min.js';
+      expect(files).to.be.an(Object);
+      expect(files.js).to.be.an(Array);
+      pathToJquery = require.resolve(pathToJquery);
+      expect(files.js).to.contain(pathToJquery);
+    });
+
+    it('should handle glob files', function () {
+      cd('globs');
+      var files = getModule();
+      expect(files).to.be.an(Object);
+      ['eot', 'svg', 'ttf', 'woff'].map(function (fontExt) {
+        expect(files[fontExt]).to.be.an(Array);
+        expect(files[fontExt][0]).to.eql(path.resolve('./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.' + fontExt));
+      });
+    });
+
+    it('should get list of files without being split by extension', function () {
+      cd('just-array');
+      var files = getModule({
+        json: path.join(__dirname, 'just-array/bower.json'),
+        ext: false
+      });
+      expect(files).to.be.an(Array);
+      expect(files.length).to.be(3);
+    });
+
+    it('should get only the files with a certain extension', function () {
+      cd('just-one-ext');
+      var files = getModule({ext: 'css'});
+      expect(files).to.be.an(Array);
+      expect(files.length).to.be(1);
+      expect(files[0]).be.equal(path.join(
+        __dirname,
+        '/just-one-ext/bower_components/bootstrap/dist/css/bootstrap.css'
+      ));
+    });
+
+    it('should get devDependencies when option is given', function () {
+      cd('dev-dependencies');
+      var files = getModule({
+        dev: true,
+        ext: 'js'
+      });
+      expect(files).to.be.an(Array);
+      expect(files.length).to.be(4);
+    });
+
+    it('should not get devDependencies when option is not given', function () {
+      cd('dev-dependencies');
+      var files = getModule({ext: 'js'});
+      expect(files).to.be.an(Array);
+      expect(files.length).to.be(3);
+    });
+
+    it('should follow symbolic links', function () {
+      var source     = path.join(__dirname, 'symlinks/angular');
+      var dest       = path.join(__dirname, 'symlinks/bower_components/angular');
+      var expectedJS = path.join(__dirname, 'symlinks/bower_components/angular/angular.js');
+      try {
+        fs.symlinkSync(source, dest);
+        cd('symlinks');
+        var files = getModule({ext: 'js'});
+        expect(files).to.be.an(Array);
+        expect(files.length).to.be(1);
+        expect(files[0]).to.be(expectedJS);
+      } catch(err) {
+        throw err;
+      } finally {
+        fs.unlinkSync(dest);
+      }
+    });
   });
 
+
   describe('Level 2', function () {
-    it('handles symbolic links', symbolicLinks);
-    it('handles no bower.json', noBower);
-    it('handles missing dependencies', missingDependencies);
-    it('handles no main property', noMain);
+
+    it('fails with no bower.json', function () {
+      cd('no-bower');
+      var error;
+      try { var files = getModule(); }
+      catch (e) { error = e; }
+      expect(error).to.be.an(Error);
+    });
+
+    it('handles missing dependencies', function () {
+      cd('missing-dependencies');
+      var error;
+      try { var files = getModule(); }
+      catch (e) { error = e; }
+      expect(error).to.be.an(Error);
+      error = null;
+
+      deleteCache('index.js');
+      cd('missing-child-dependencies');
+      try { var files = getModule(); }
+      catch (e) { error = e; }
+      expect(error).to.be.an(Error);
+      error = null;
+    });
+
+    it('handles no main property', function () {
+      cd('no-main');
+      var error;
+      try { var files = getModule(); }
+      catch (e) { error = e; }
+      expect(error).to.be.an(Error);
+    });
   });
 
 });
@@ -46,154 +142,4 @@ function getModule(options) {
 }
 function deleteCache(filepath) {
   delete require.cache[require.resolve('../lib/' + filepath)];
-}
-
-// override
-// --------
-function override() {
-  cd('override');
-  var files        = getModule()
-    , pathToJquery = './override/bower_components/jquery/dist/jquery.min.js'
-    ;
-
-  should(files).be.ok;
-  should(files).be.an.Object;
-  should(files).have.property('js');
-  should(files.js).be.an.Array;
-  pathToJquery = require.resolve(pathToJquery);
-  should(files.js).containEql(pathToJquery);
-}
-
-// globs
-// -----
-function globs() {
-  cd('globs');
-  var files = getModule();
-
-  should(files).be.ok;
-  should(files).be.an.Object;
-  ['eot', 'svg', 'ttf', 'woff'].map(function (fontExt) {
-    should(files).have.property(fontExt);
-    should(files[fontExt]).be.an.Array;
-    should(files[fontExt][0]).eql(path.resolve('./bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.' + fontExt));
-  });
-}
-
-// justArray
-// ---------
-function justArray() {
-  cd('just-array');
-  var files = getModule({
-    json: path.join(__dirname, 'just-array/bower.json'),
-    ext: false
-  });
-
-  should(files).be.ok;
-  should(files).be.an.Array;
-  should(files).have.lengthOf(3);
-}
-
-// justOneExt
-// ----------
-function justOneExt() {
-  cd('just-one-ext');
-  var files = getModule({
-    ext: 'css'
-  });
-
-  should(files).be.ok;
-  should(files).be.an.Array;
-  should(files).have.lengthOf(1);
-  should(files[0]).be.equal(path.join(
-    __dirname,
-    '/just-one-ext/bower_components/bootstrap/dist/css/bootstrap.css'
-  ));
-}
-
-// devDependencies
-// ---------------
-function devDependencies() {
-  cd('dev-dependencies');
-  var files = getModule({
-    dev: true,
-    ext: 'js'
-  });
-
-  should(files).be.ok;
-  should(files).be.an.Array;
-  should(files).have.lengthOf(4);
-}
-
-// noDevDependencies
-// ---------------
-function noDevDependencies() {
-  cd('dev-dependencies');
-  var files = getModule({
-    ext: 'js'
-  });
-
-  should(files).be.ok;
-  should(files).be.an.Array;
-  should(files).have.lengthOf(3);
-}
-
-// noBower
-// -------
-function noBower() {
-  cd('no-bower');
-  var error;
-  try { var files = getModule(); }
-  catch (e) { error = e; }
-  should(error).be.an.Error;
-}
-
-// symbolicLinks
-// -------------
-function symbolicLinks() {
-  var source     = path.join(__dirname, 'symlinks/angular');
-  var dest       = path.join(__dirname, 'symlinks/bower_components/angular');
-  var expectedJS = path.join(__dirname, 'symlinks/bower_components/angular/angular.js');
-
-  try {
-    fs.symlinkSync(source, dest);
-    cd('symlinks');
-    var files = getModule({ext: 'js'});
-
-    should(files).be.ok;
-    should(files).be.an.Array;
-    should(files).have.lengthOf(1);
-    should(files[0]).be.equal(expectedJS);
-  } catch(err) {
-    throw err;
-  } finally {
-    fs.unlinkSync(dest);
-  }
-};
-
-// missingDependencies
-// -------------------
-function missingDependencies() {
-  cd('missing-dependencies');
-  var error;
-  try { var files = getModule(); }
-  catch (e) { error = e; }
-  should(error).be.an.Error;
-  error = null;
-
-  deleteCache('bower-files.js');
-  cd('missing-child-dependencies');
-  try { var files = getModule(); }
-  catch (e) { error = e; }
-  should(error).be.an.Error;
-  error = null;
-}
-
-// noMain
-// ------
-function noMain() {
-  cd('no-main');
-  var error;
-  try { var files = getModule(); }
-  catch (e) { error = e; }
-  should(error).be.an.Error;
 }
