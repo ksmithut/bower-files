9103f371e07a574a83b2cf749acd6d47883c43c0
ksmithut <ksmith.ut@gmail.com>
2015-01-06 13:36:57 -0700

    fixed index.js temporarily

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ var BowerFiles = require('./lib/bower-files');
 
 module.exports = function (options) {
   var bowerFiles = new BowerFiles(options);
-  var files      = files.getFiles(options);
-  var components = files.getComponents();
-  files.get = function (componentName, options) {
-    if (!components[componentName]) { return; }
-    return components[componentName].getFiles(options);
-  };
-  return files;
+  return bowerFiles.getFiles(options);
 };
 
