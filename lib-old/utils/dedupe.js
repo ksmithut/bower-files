f18f4ecdfd145859ea6a2e09f6a6fca7f445ef18
ksmithut <ksmith.ut@gmail.com>
2015-01-07 12:47:14 -0700

    removed old code

--- lib-old/utils/dedupe.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var arrayify = require('./arrayify');
-
-module.exports = function (arr) {
-  return arrayify(arr).filter(function (item, i) {
-    return arr.indexOf(item) === i;
-  });
-};
--- lib-old/utils/read-json.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-
-module.exports = function (path) {
-  if (!fs.existsSync(path)) { return {}; }
-  return JSON.parse(fs.readFileSync(path, 'utf8'));
-};
